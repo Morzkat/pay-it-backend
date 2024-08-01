2fe487f2407a0e0592ffce2e6bef1bd8f5326582
Morzkat <felix.david21@outlook.com>
2024-08-01 13:53:09 -0400

    * Move migrations folder to root directory. * Add new folders and files to gitignore.  * Update ts configs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     schema: './src/db/drizzle/schemas.ts',
-    out: './src/db/drizzle/migrations',
+    out: './migrations',
     dialect: 'postgresql',
     dbCredentials: {
       url: 'postgresql://postgres:0ec10cf380c8426b8ca0c417fe0ad07b@localhost:5432/pay-it-db',
